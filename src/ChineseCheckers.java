b8e4f82329167fcc180ce3ba203a1c4b0624ccda
Jonathan <jonathanxu25@gmail.com>
2019-04-24 15:32:52 -0400

    added method to output valid jumps

--- src/ChineseCheckers.java
+++ src/ChineseCheckers.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ChineseCheckers {
   public static void main(String[] args) {
     //Creates board
@@ -16,6 +18,35 @@ public class ChineseCheckers {
       }
     }
 
-    
+  }
+
+  public ArrayList<int[]> nextAvailableMoves (int r1, int c1, int[][] board){
+    ArrayList<int[]> moves = new ArrayList<>();
+    int[] move = new int[2];
+    // Checks adjacent moves and jump moves
+    for(int i = -1; i <= 1; i++){
+      for(int j = -1; j <= 1; j++){
+        if(r1+i >= 9 && r1+i <= 25 && c1+j >= 1 && c1+j <= 17){ // If in board
+          if(board[r1+i][c1+j] == 0){ // If adjacent is empty
+            move = {r1+i, c1+j};
+            moves.add(move);
+          } else if(i != 0 && j != 0){
+            if(r1+2i >= 9 && r1+2i <= 25 && c1+2j >= 1 && c1+2j <= 17){ // If in board
+              if(board[r1+2i][c1+2j] == 0) { // If jump is empty
+                move = {r1+2i, c1+2j};
+                moves.add(move);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return moves;
+
+  }
+
+  public int checkAdjacent(int r, int c){
+    return 0;
   }
 }
